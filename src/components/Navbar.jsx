4369dd677920d1b723fa21d56c6fd98ccd1ce1fa
Rakib <hellorakibhossen@gmail.com>
2025-04-02 09:14:50 +0600

    navbar create done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const routes = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Job", path: "/job" },
+    { name: "Service", path: "/service" },
+    { name: "Community", path: "/community" },
+  ];
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div className="my-4">
+      <div className="flex justify-between items-center max-w-[1100px] w-11/12 mx-auto bg-white p-2.5  border border-gray-200 rounded-full shadow-lg">
+        <div className="lg:ml-5 flex items-center gap-2 relative">
+          <RxHamburgerMenu
+            onClick={() => setOpen(!open)}
+            className="text-xl lg:hidden cursor-pointer"
+          />
+          {open && (
+            <ul className="flex flex-col  w-44 absolute border border-gray-200 bg-white p-3 top-16 rounded-2xl">
+              {routes.map((route) => (
+                <li
+                  key={route.path}
+                  className="text-sm hover:bg-gray-100 p-1.5 rounded-sm "
+                >
+                  <Link href={route.path} className="text-gray-700">
+                    {route.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+          <h3 className="text-3xl font-semibold text-violet-700  ">devmeet</h3>
+        </div>
+        <ul className="lg:flex items-center gap-4 hidden ">
+          {routes.map((route) => (
+            <li key={route.path}>
+              <Link href={route.path} className="text-gray-700">
+                {route.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Link href="signIn">
+          <button className="border py-1 px-3 font-semibold text-gray-600 rounded-full cursor-pointer border-gray-200 ">
+            Sign in
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
